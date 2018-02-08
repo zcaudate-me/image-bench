e5e50e3aadf9f41b8a5d83e99fce57f4706f089b
Chris Zheng <z@caudate.me>
2018-02-08 21:55:03 +0530

    removing javacpp deps

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://gitlab.com/zcaudate/image-bench"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [uncomplicate/clojurecl "0.8.0"]
-                 [org.bytedeco/javacv "1.4"]
-                 [org.bytedeco/javacv-platform "1.4"]
-                 [org.bytedeco.javacpp-presets/opencv "3.4.0-1.4"]
-                 [org.bytedeco.javacpp-presets/opencv "3.4.0-1.4" :classifier "macosx-x86_64"]
-                 [org.bytedeco.javacpp-presets/opencv-platform "3.4.0-1.4"]
+                 ;;[org.bytedeco/javacv "1.4"]
+                 ;;[org.bytedeco/javacv-platform "1.4"]
+                 ;;[org.bytedeco.javacpp-presets/opencv "3.4.0-1.4"]
+                 ;;[org.bytedeco.javacpp-presets/opencv "3.4.0-1.4" :classifier "macosx-x86_64"]
+                 ;;[org.bytedeco.javacpp-presets/opencv-platform "3.4.0-1.4"]
                  [com.twelvemonkeys.imageio/imageio-tiff "3.3.2"]]
                  
   :aliases {"test" ["run" "-m" "hara.test" "exit"]}
