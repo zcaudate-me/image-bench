09a99dd96116597abd0d0e06267f230d2df0ac2b
Chris Zheng <z@caudate.me>
2018-02-08 23:04:44 +0530

    added sobel test

--- test/clojure/image_bench/sobel_test.clj
+++ test/clojure/image_bench/sobel_test.clj
@@ -15,9 +15,19 @@
 
 
 ;; FAILS
-(fact "running a sobel kernel at [345, 912] [13, 3]"
+(fact "running a grayscale kernel at [345, 912] [13, 3]"
 
   (def img (img/load-image "resources/lena-0345x0912.jpg"))
   
-  (img/display-image (gray/grayscale img [13 3]))
+  (gray/grayscale img [13 3])
+  => java.awt.image.BufferedImage)
+
+
+
+;; FAILS
+(fact "running a sobel kernel at [345, 912] [13, 3]"
+
+  (def img (img/load-image "resources/lena-0345x0912.jpg"))
+
+  (sobel/sobel-invalid-work-group-error img [13 3])
   => java.awt.image.BufferedImage)
